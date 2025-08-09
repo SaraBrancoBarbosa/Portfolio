935b7c98bddb61c69cd3a82a55fcc182360aba50
SaraBrancoBarbosa <165152164+SaraBrancoBarbosa@users.noreply.github.com>
2025-08-09 13:40:45 +0200

    CSS update + responsive, text title

--- src/pages/projects/Projects.jsx
+++ src/pages/projects/Projects.jsx
@@ -2,16 +2,14 @@ import { Link } from "react-router"
 
 function Projects() {
   return (
-    <div className="w-full">
+    <div className="flex flex-col gap-5 lg:gap-10 w-full">
       
-      <div className="bg-blue-500">
-        <h1>Mes projets</h1>
-
-        <h2>Cette section regroupe les informations sur mes projets.</h2>
+      <div className="mt-3 sm:mt-8">
+        <h1 className="text-4xl md:text-5xl">Mes projets</h1>
       </div>
 
-      <div className="flex flex-col-reverse sm:grid sm:grid-cols-2 gap-10 p-10 justify-center items-center bg-blue-200">
-        <div className="flex flex-col items-center text-center gap-5">
+      <div className="flex flex-col-reverse sm:grid sm:grid-cols-2 gap-10 p-10 justify-center items-center rounded-2xl bg-violet-100 sm:min-h-104">
+        <div className="flex flex-col items-center text-center gap-5 p-6 rounded-2xl bg-white">
           <p>Cette section regroupe une sélection de sites web que j'ai codé dans différents contextes (formations, stages, projets personnels...).</p>
           <p>Tous mes autres projets se trouvent dans <a href="https://github.com/SaraBrancoBarbosa" className="underline">ma page GitHub !</a></p>
           <Link to="/projects/websites">
@@ -26,12 +24,12 @@ function Projects() {
         </div>
       </div>
 
-      <div className="grid grid-cols-1 sm:grid-cols-2 gap-10 p-10 justify-center items-center bg-green-300">
+      <div className="grid grid-cols-1 sm:grid-cols-2 mb-5 sm:mb-6 gap-10 p-10 justify-center items-center rounded-2xl bg-violet-100 sm:min-h-104">
         <div className="flex flex-col items-center">
           <img src="assets/projects/projectVideos.png" className="w-64 sm:w-120" alt="Image d'illustration pour la section vidéos." />
         </div>
 
-        <div className="flex flex-col items-center text-center gap-5">
+        <div className="flex flex-col items-center text-center gap-5 p-6 rounded-2xl bg-white">
           <p>Cette section regroupe les vidéos que j'ai monté pour mon Master Audiovisuel, Médias Interactifs Numériques et Jeux, et pour des projets personnels.</p>
           <p>Tous mes projets se trouvent dans <a href="https://www.youtube.com/@SaraBrancoBarbosa" className="underline">ma page YouTube !</a></p>
           <Link to="/projects/videos">
