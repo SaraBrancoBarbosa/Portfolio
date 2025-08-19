be471301a021e4a3796547f56e3d06fa46a9d5c7
SaraBrancoBarbosa <165152164+SaraBrancoBarbosa@users.noreply.github.com>
2025-08-19 18:38:59 +0200

    Scroll to top component added to scroll when changing page

--- src/Router.jsx
+++ src/Router.jsx
@@ -5,10 +5,12 @@ import Error from "./pages/error/Error"
 import Projects from "./pages/projects/Projects"
 import Websites from "./pages/projects/Websites"
 import Videos from "./pages/projects/Videos"
+import ScrollToTop from "./components/scrollToTop/ScrollToTop"
 
 function AppRouter() {
   return (
     <HashRouter>
+      <ScrollToTop />
         <Routes>
             <Route path="/" element={<Layout />}>
                 <Route index element={<Homepage />} />
