ee4173ff72c092d5f689159888f9d89e8c12de65
SaraBrancoBarbosa <165152164+SaraBrancoBarbosa@users.noreply.github.com>
2025-08-20 10:42:16 +0200

    Changing assets paths for deployment

--- src/pages/projects/Videos.jsx
+++ src/pages/projects/Videos.jsx
@@ -15,7 +15,7 @@ function Videos() {
 
       <VideosContainer
         background="#fee2e2"
-        video={`/assets/videos/SetMeFree.mp4`}
+        video={`assets/videos/SetMeFree.mp4`}
         thumbnail="assets/videos/SetMeFreeThumbnail.jpg"
         youtubeLink={`https://youtu.be/DE2VneeHYYY?si=T62lsHc-3iApYgsg`}
         type="Vidéo promotionnelle d’une application mobile (fictive)"
@@ -23,14 +23,14 @@ function Videos() {
         tools=
           {`Canva`}
         toolsLogo={[
-          { name: "Canva", src: "/assets/icons/tools/canva.svg" },
+          { name: "Canva", src: "assets/icons/tools/canva.svg" },
         ]}
         presentation="Cette vidéo accompagne tout un projet de création d’une application mobile : sa création, son interface graphique, la définition des fonctionnalités, sa mise en ligne... La vidéo joue sur l'ironie, avec une musique pleine d'espoir pour une situation en réalité assez cocasse."
       />
 
       <VideosContainer
         background="#cffafe"
-        video={`/assets/videos/Mouche.mp4`}
+        video={`assets/videos/Mouche.mp4`}
         thumbnail="assets/videos/MoucheThumbnail.jpg"
         youtubeLink={`https://youtube.com/shorts/7K1SozXF3QU?si=30UnH31Mv_3o7e_j`}
         type="Animation en boucle"
@@ -40,16 +40,16 @@ function Videos() {
           Adobe Animate
           Adobe Première Pro`}
         toolsLogo={[
-          { name: "Adobe Illustrator", src: "/assets/icons/tools/adobe-illustrator.svg" },
-          { name: "Adobe Animate", src: "/assets/icons/tools/adobe-animate.svg" },
-          { name: "Adobe Première Pro", src: "/assets/icons/tools/adobe-premiere.svg" },
+          { name: "Adobe Illustrator", src: "assets/icons/tools/adobe-illustrator.svg" },
+          { name: "Adobe Animate", src: "assets/icons/tools/adobe-animate.svg" },
+          { name: "Adobe Première Pro", src: "assets/icons/tools/adobe-premiere.svg" },
         ]}
         presentation="Vidéo humoristique avec plusieurs éléments : changements de scènes, animation « loop », déplacements du personnage…"
       /> 
 
       <VideosContainer
         background="#fef3c7"
-        video={`/assets/videos/Mabele.mp4`}
+        video={`assets/videos/Mabele.mp4`}
         thumbnail="assets/videos/MabeleThumbnail.jpg"
         youtubeLink={`https://youtu.be/poMUwkWI6lc?si=OXCSKQSIBdbde6Yz`}
         type="Présentation et comparaison détaillée de deux CMS, PrestaShop et Wordpress (WooCommerce)"
@@ -57,7 +57,7 @@ function Videos() {
         tools=
           {`Adobe Première Pro`}
         toolsLogo={[
-          { name: "Adobe Première Pro", src: "/assets/icons/tools/adobe-premiere.svg" },
+          { name: "Adobe Première Pro", src: "assets/icons/tools/adobe-premiere.svg" },
         ]}
         presentation="Vidéo qui présente et compare deux CMS permettant de réaliser du commerce en ligne. Le contenu a été réalisé en binôme avec Adrien Cauchy (prises d’écran, textes…) et la vidéo montée par moi-même."
       />
@@ -65,7 +65,7 @@ function Videos() {
       <div className="mb-5 lg:mb-10">
         <VideosContainer
           background="#dcfce7"
-          video={`/assets/videos/BelfortTourisme.mp4`}
+          video={`assets/videos/BelfortTourisme.mp4`}
           thumbnail="assets/videos/BelfortTourismeThumbnail.jpg"
           youtubeLink={`https://youtu.be/q4TZETGRGgE?si=rMREZocj-R_MdfXR`}
           type="Présentation de la ville de Belfort et de son tourisme"
@@ -73,7 +73,7 @@ function Videos() {
           tools=
             {`Sony Vegas Pro 16`}
           toolsLogo={[
-            { name: "Sony Vegas Pro 16", src: "/assets/icons/tools/sony-vegas.svg" }
+            { name: "Sony Vegas Pro 16", src: "assets/icons/tools/sony-vegas.svg" }
           ]}
           presentation="Cette vidéo avait pour but d’être présentée à des apprenants FLE de niveau intermédiaire. J’ai aidé mon ami dans ce projet, qui a été réalisé en peu de temps et avec les moyens du bord !
             Script et mise en scène : Jason Roeum.
--- src/pages/projects/Websites.jsx
+++ src/pages/projects/Websites.jsx
@@ -14,7 +14,7 @@ function Websites() {
       </div>
 
         <WebsitesContainer
-          background="/assets/projects/backgrounds/hrnetBg.webp"
+          background="assets/projects/backgrounds/hrnetBg.webp"
           image="assets/projects/hrnetLogo.webp"
           title="HRnet"
           type="Réalisation d’un site web pour la société financière WealthHealth (entreprise fictive)"
@@ -37,17 +37,17 @@ function Websites() {
             Sass
             Jest`}
           toolsLogo={[
-            { name: "Node", src: "/assets/icons/tools/node.svg" },
-            { name: "React", src: "/assets/icons/tools/react.svg" },
-            { name: "Vite", src: "/assets/icons/tools/vite.svg" },
-            { name: "Router", src: "/assets/icons/tools/router.svg" },
-            { name: "Sass", src: "/assets/icons/tools/sass.svg" },
-            { name: "Jest", src: "/assets/icons/tools/jest.svg" },
+            { name: "Node", src: "assets/icons/tools/node.svg" },
+            { name: "React", src: "assets/icons/tools/react.svg" },
+            { name: "Vite", src: "assets/icons/tools/vite.svg" },
+            { name: "Router", src: "assets/icons/tools/router.svg" },
+            { name: "Sass", src: "assets/icons/tools/sass.svg" },
+            { name: "Jest", src: "assets/icons/tools/jest.svg" },
           ]}  
         />  
       
         <WebsitesContainer
-          background="/assets/projects/backgrounds/argentBankBg.webp"
+          background="assets/projects/backgrounds/argentBankBg.webp"
           image="assets/projects/argentBankLogo.webp"
           title="Argent Bank"
           type="Réalisation d’un site web pour la banque Argent Bank (entreprise fictive)"
@@ -67,18 +67,18 @@ function Websites() {
             Redux Toolkit
             MongoDB`}
           toolsLogo={[
-            { name: "Node", src: "/assets/icons/tools/node.svg" },
-            { name: "React", src: "/assets/icons/tools/react.svg" },
-            { name: "Vite", src: "/assets/icons/tools/vite.svg" },
-            { name: "Router", src: "/assets/icons/tools/router.svg" },
-            { name: "Sass", src: "/assets/icons/tools/sass.svg" },
-            { name: "Redux", src: "/assets/icons/tools/redux.svg" },
-            { name: "MongoDB", src: "/assets/icons/tools/mongodb.svg" },
+            { name: "Node", src: "assets/icons/tools/node.svg" },
+            { name: "React", src: "assets/icons/tools/react.svg" },
+            { name: "Vite", src: "assets/icons/tools/vite.svg" },
+            { name: "Router", src: "assets/icons/tools/router.svg" },
+            { name: "Sass", src: "assets/icons/tools/sass.svg" },
+            { name: "Redux", src: "assets/icons/tools/redux.svg" },
+            { name: "MongoDB", src: "assets/icons/tools/mongodb.svg" },
           ]}
         />  
       
         <WebsitesContainer
-          background="/assets/projects/backgrounds/fannynjaBg.webp"
+          background="assets/projects/backgrounds/fannynjaBg.webp"
           image="assets/projects/fannynjaLogo.webp"
           title="Les personnages de Fanny"
           type={
@@ -112,17 +112,17 @@ function Websites() {
             Sass
             PaintTool Sai 2 (illustrations)`}
           toolsLogo={[
-            { name: "Node", src: "/assets/icons/tools/node.svg" },
-            { name: "React", src: "/assets/icons/tools/react.svg" },
-            { name: "Vite", src: "/assets/icons/tools/vite.svg" },
-            { name: "Router", src: "/assets/icons/tools/router.svg" },
-            { name: "Sass", src: "/assets/icons/tools/sass.svg" },
-            { name: "PaintTool Sai 2", src: "/assets/icons/tools/sai.svg" },
+            { name: "Node", src: "assets/icons/tools/node.svg" },
+            { name: "React", src: "assets/icons/tools/react.svg" },
+            { name: "Vite", src: "assets/icons/tools/vite.svg" },
+            { name: "Router", src: "assets/icons/tools/router.svg" },
+            { name: "Sass", src: "assets/icons/tools/sass.svg" },
+            { name: "PaintTool Sai 2", src: "assets/icons/tools/sai.svg" },
           ]}
         />  
 
         <WebsitesContainer
-          background="/assets/projects/backgrounds/roulcoolsBg.webp"
+          background="assets/projects/backgrounds/roulcoolsBg.webp"
           image="assets/projects/roulcoolsLogo.webp"
           title="Association Roulcools Bike Elzange"
           type="Réalisation d’un site web et d'un forum pour le club de VTT pour enfants et adultes Roulcools Bike"
@@ -144,15 +144,15 @@ function Websites() {
             phpBB
             o2switch (hébergement)`}
           toolsLogo={[
-            { name: "Wordpress", src: "/assets/icons/tools/wordpress.svg" },
-            { name: "phpBB", src: "/assets/icons/tools/phpbb.svg" },
-            { name: "o2switch", src: "/assets/icons/tools/o2switch.svg" },
+            { name: "Wordpress", src: "assets/icons/tools/wordpress.svg" },
+            { name: "phpBB", src: "assets/icons/tools/phpbb.svg" },
+            { name: "o2switch", src: "assets/icons/tools/o2switch.svg" },
           ]}
         />
       
         <div className="mb-5 lg:mb-10">
           <WebsitesContainer
-            background="/assets/projects/backgrounds/econceptBg.webp"
+            background="assets/projects/backgrounds/econceptBg.webp"
             image="assets/projects/econceptLogo.webp"
             title="E-Concept Applications"
             type="Refonte du site web de l'ntreprise de services informatiques pour particuliers et professionnels E-Concept Applications"
@@ -170,8 +170,8 @@ function Websites() {
               Affinity Designer
               Hébergement chez l’entreprise`}
             toolsLogo={[
-              { name: "Bootstrap", src: "/assets/icons/tools/bootstrap.svg" },
-              { name: "Affinity Designer", src: "/assets/icons/tools/affinity.svg" },
+              { name: "Bootstrap", src: "assets/icons/tools/bootstrap.svg" },
+              { name: "Affinity Designer", src: "assets/icons/tools/affinity.svg" },
             ]}
           />
         </div>
